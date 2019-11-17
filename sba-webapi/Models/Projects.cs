a2c0037e9ed79fc1e5c45da2442988ff66ce4832
Manickaraj-stack <56302785+Manickaraj-stack@users.noreply.github.com>
2019-11-17 23:16:00 +0530

    projects model added

---
+++ sba-webapi/Models/Projects.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projectmanager.Models
+{
+    public class Projects
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public Nullable<bool> IsSetdate { get; set; }
+        public int ID { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int ProjectPriority { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
