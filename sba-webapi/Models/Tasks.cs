c1f67a4a1d18425d1566b1b18ed54f5ab338476a
Manickaraj-stack <56302785+Manickaraj-stack@users.noreply.github.com>
2019-11-17 23:16:25 +0530

    tasks model added

---
+++ sba-webapi/Models/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projectmanager.Models
+{
+    public class Tasks
+    {
+        public int TaskId { get; set; }
+        public int ProjectId { get; set; }
+        public int ParentId { get; set; }
+        public Nullable<bool> IsParentTask { get; set; }
+        public string TaskName { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int UserId { get; set; }
+        public int TaskPriority { get; set; }
+
+        public virtual ParentTask ParentTask { get; set; }
+        public virtual Project Project { get; set; }
+        public virtual User User { get; set; }
+    }
+}
